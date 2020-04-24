f5f3b68c58a1131080338c6289dbd233c5d0e402
jy <ltmzy2@163.com>
2020-04-24 17:45:21 +0800

    初始化develop分支

--- src/main/java/com/jy/x/GenApplication.java
+++ src/main/java/com/jy/x/GenApplication.java
@@ -2,17 +2,9 @@ package com.jy.x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GenApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // TODO Auto-generated method stub
-        return builder.sources(GenApplication.class);
-    }
+public class GenApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(GenApplication.class, args);
